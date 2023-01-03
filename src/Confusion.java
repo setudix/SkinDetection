552f5df42cb8ee19e489bec12bdfdfe4024c6638
setudix <77272948+setudix@users.noreply.github.com>
2023-01-03 16:37:05 +0600

    k-fold

--- src/Confusion.java
+++ src/Confusion.java
@@ -6,15 +6,15 @@ import java.io.IOException;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class Confusion {
 
     private static final int SIZE = 256;
+    private static final int IMGSIZE = 1360;
     private long[][][] skinValues, nonSkinValues;
-    private boolean [][][][] isColorSkin;
+    private boolean [][][] isPixelSkin;
     long totalSkinCount, totalNonSkinCount;
     BigDecimal totalSkinCountBig, totalNonSkinCountBig, thresholdBig;
     ArrayList<String> maskImageNames;
@@ -23,7 +23,7 @@ public class Confusion {
     Confusion(){
         skinValues = new long[SIZE][SIZE][SIZE];
         nonSkinValues = new long[SIZE][SIZE][SIZE];
-        isColorSkin = new boolean[60][SIZE][SIZE][SIZE];
+        isPixelSkin = new boolean[60][IMGSIZE][IMGSIZE];
         skinNonSkinRatio = new ArrayList<>();
         thresholdBig = new BigDecimal("0.4");
         if (maskImageNames == null){
@@ -31,11 +31,24 @@ public class Confusion {
             System.out.println("size " + maskImageNames.size());
         }
         resetValues();
-
+        Collections.shuffle(maskImageNames);
+//        int ww = 0, hh = 0;
+//        for (String name : maskImageNames){
+//            BufferedImage image = null;
+//            try {
+//                image = ImageIO.read(new File("./ibtd/Mask/".concat(name)));
+//            } catch (IOException e) {
+//                throw new RuntimeException(e);
+//            }
+//            ww = Integer.max(ww, image.getWidth());
+//            hh = Integer.max(hh, image.getHeight());
+//        }
+//        System.out.println(ww + " " + hh);
     }
 
-    public void train() throws IOException {
-        for (int i=0;i<500;i++){
+    public void train(int run) throws IOException {
+        for (int i=0;i<555;i++){
+
             String maskName = maskImageNames.get(i);
             String nonMaskName = maskName.replaceAll("bmp", "jpg");
 
@@ -48,19 +61,22 @@ public class Confusion {
             BufferedImage maskImage = ImageIO.read(maskImageFile);
             BufferedImage nonmaskImage = ImageIO.read(nonmaskImageFile);
 
-            for (int h=0;h<maskImage.getHeight();h++){
-                for (int w=0;w<maskImage.getWidth();w++) {
+            boolean test_set = (run*55 <= i && i < (run+1)*55);
+//            System.out.println(maskImage.getHeight() + " -- " + maskImage.getWidth());
+            for (int h = 0; h < maskImage.getHeight(); h++) {
+                for (int w = 0; w < maskImage.getWidth(); w++) {
 
-                    int maskPixel = maskImage.getRGB(w,h);
+                    int maskPixel = maskImage.getRGB(w, h);
                     Color maskColor = new Color(maskPixel, true);
 
                     int r = maskColor.getRed();
                     int g = maskColor.getGreen();
                     int b = maskColor.getBlue();
 
-                    if (r > 240 && g > 240 && b > 240){
 
-                        int nonmaskPixel = nonmaskImage.getRGB(w,h);
+                    if (r > 240 && g > 240 && b > 240) {
+
+                        int nonmaskPixel = nonmaskImage.getRGB(w, h);
 
                         Color nonmaskColor = new Color(nonmaskPixel, true);
 
@@ -68,12 +84,23 @@ public class Confusion {
                         int g_nm = nonmaskColor.getGreen();
                         int b_nm = nonmaskColor.getBlue();
 
+                        if (test_set == true){
+//                            System.out.println(h + " " + w);
+                            isPixelSkin[i%55][h][w] = false;
+                            continue;
+                        }
+
                         nonSkinValues[r_nm][g_nm][b_nm]++;
                         totalNonSkinCount++;
                     } else {
+                        if (test_set == true){
+                            isPixelSkin[i%55][h][w] = true;
+                            continue;
+                        }
                         skinValues[r][g][b]++;
                         totalSkinCount++;
                     }
+
                 }
             }
         }
@@ -88,11 +115,7 @@ public class Confusion {
                         skin = BigDecimal.valueOf(skinValues[r][g][b]);
                         skin_p = skin.divide(totalSkinCountBig, 20, RoundingMode.CEILING);
                     } else {
-                        if (nonSkinValues[r][g][b] == 0){
-                            skinNonSkinRatio.add(BigDecimal.valueOf(0));
-                        } else {
-                            skinNonSkinRatio.add(BigDecimal.valueOf(1));
-                        }
+                        skinNonSkinRatio.add(BigDecimal.valueOf(0));
                         continue;
                     }
                     if (nonSkinValues[r][g][b] != 0){
@@ -112,32 +135,33 @@ public class Confusion {
                 }
             }
         }
-        for (int i=500;i<maskImageNames.size();i++){
-            String nonmaskName = maskImageNames.get(i).replaceAll("bmp","jpg");
-            BufferedImage image = ImageIO.read(new File("./ibtd/Nonmask/".concat(nonmaskName)));
-            for (int h=0;h<image.getHeight();h++){
-                for (int w=0;w<image.getWidth();w++) {
-
-                    int pixel = image.getRGB(w,h);
-                    Color color = new Color(pixel, true);
-
-                    int r = color.getRed();
-                    int g = color.getGreen();
-                    int b = color.getBlue();
-
-                    if (r > 240 && g > 240 && b > 240){
-                        isColorSkin[i%500][r][g][b] = false;
-                    } else {
-                        isColorSkin[i%500][r][g][b] = true;
-                    }
-                }
-            }
-        }
+//        for (int i=run*55;i<(run+1)*55;i++){
+////            String nonmaskName = maskImageNames.get(i).replaceAll("bmp","jpg");
+////            BufferedImage image = ImageIO.read(new File("./ibtd/Nonmask/".concat(nonmaskName)));
+//            BufferedImage image = ImageIO.read(new File("./ibtd/Mask/".concat(maskImageNames.get(i))));
+//            for (int h=0;h<image.getHeight();h++){
+//                for (int w=0;w<image.getWidth();w++) {
+//
+//                    int pixel = image.getRGB(w,h);
+//                    Color color = new Color(pixel, true);
+//
+//                    int r = color.getRed();
+//                    int g = color.getGreen();
+//                    int b = color.getBlue();
+//
+//                    if (r > 240 && g > 240 && b > 240){
+//                        isColorSkin[i%55][r][g][b] = false;
+//                    } else {
+//                        isColorSkin[i%55][r][g][b] = true;
+//                    }
+//                }
+//            }
+//        }
     }
 
     private void test(int run) throws IOException {
         int testTotal = 0;
-        for (int i=500;i<maskImageNames.size();i++){
+        for (int i=run*55;i<(run+1)*55;i++){
 
             String nonmaskName = maskImageNames.get(i).replaceAll("bmp","jpg");
             BufferedImage image = ImageIO.read(new File("./ibtd/Nonmask/".concat(nonmaskName)));
@@ -154,11 +178,11 @@ public class Confusion {
                     int index = SIZE*(SIZE*r + g) + b;
 
                     if (skinNonSkinRatio.get(index).compareTo(thresholdBig) >= 0) {
-                        if (isColorSkin[i%500][r][g][b] == true) {
+                        if (isPixelSkin[i%55][h][w] == true) {
                             TP++;
                         }
                     } else {
-                        if (isColorSkin[i%500][r][g][b] == false){
+                        if (isPixelSkin[i%55][h][w] == false){
                             TN++;
                         }
                     }
@@ -170,7 +194,7 @@ public class Confusion {
         BigDecimal totalCorrectBig = BigDecimal.valueOf(TN + TP);
         BigDecimal accuracy = totalCorrectBig.divide(totalBig, 20, RoundingMode.CEILING);
 
-        System.out.println(run + ". Accuracy = " + accuracy);
+        System.out.println(run+1 + ". Accuracy = " + accuracy);
     }
     private void resetValues(){
         for (int r=0;r<SIZE;r++){
@@ -183,13 +207,19 @@ public class Confusion {
         }
 //        skinValues = new long[SIZE][SIZE][SIZE];
 //        nonSkinValues = new long[SIZE][SIZE][SIZE];
+        for (int i=0;i<60;i++){
+            for (int j=0;j<IMGSIZE;j++){
+                for (int k=0;k<IMGSIZE;k++){
+                    isPixelSkin[i][j][k] = false;
+                }
+            }
+        }
         skinNonSkinRatio.clear();
         totalNonSkinCount = 0;
         totalSkinCount = 0;
         TP = 0;
         TN = 0;
-        Collections.shuffle(maskImageNames);
-        System.out.println("shuffled");
+
     }
     public void clear(){
         resetValues();
@@ -201,8 +231,8 @@ public class Confusion {
         long readingTimeEnd;
 
         for (int i=0;i<10;i++){
-            confusion.train();
-            confusion.test(i+1);
+            confusion.train(i);
+            confusion.test(i);
             confusion.clear();
             readingTimeEnd = System.currentTimeMillis();
             System.out.println("Iteration #" + (i+1) + " in "
